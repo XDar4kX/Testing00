9898b377f5095baf6e02f41d6c3ed26a7266da99
XDar4kX <navinder21@Icloud.com>
2026-05-06 14:42:26 +1000

    Update voxlblade-loader.lua

--- script/voxlblade-loader.lua
+++ script/voxlblade-loader.lua
@@ -6,13 +6,23 @@ getgenv().sharedRequire = function(path)
     path = path:gsub("^@", "")
     path = path:gsub("^/", "")
 
+    path = path:gsub("^Utils/", "utils/")
+    path = path:gsub("^utils/", "utils/")
+    path = path:gsub("^Classes/", "classes/")
+    path = path:gsub("^classes/", "classes/")
+
     if cache[path] then
         return cache[path]
     end
 
-    local source = game:HttpGet(BASE .. path)
-    local fn, err = loadstring(source, path)
+    local url = BASE .. path
+    local source = game:HttpGet(url)
 
+    if source:sub(1, 3) == "404" then
+        error("GitHub file not found: " .. url)
+    end
+
+    local fn, err = loadstring(source, path)
     if not fn then
         error(("Failed to load %s:\n%s"):format(path, err))
     end
